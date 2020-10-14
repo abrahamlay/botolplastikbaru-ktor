0863ab1f8357d540c5089d727940be1aa61bcbb4
Abraham Lay <abraham_lay@yahoo.co.id>
2020-10-15 00:28:48 +0700

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,49 @@
+package com.abraham
+
+import com.abraham.service.DatabaseHelper
+import com.abraham.service.UserService
+import com.abraham.web.widget
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.sessions.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.jackson.jackson
+import io.ktor.websocket.WebSockets
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    DatabaseHelper.init()
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(WebSockets)
+    install(ContentNegotiation){
+        jackson { configure(SerializationFeature.INDENT_OUTPUT,true)}
+    }
+    install(Routing){ widget(userService = UserService()) }
+
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+data class MySession(val count: Int = 0)
+
